03fad08cd8d904a785d09d6d775c08c272d1a3bb
Ankit K. Dixit <67652706+Ankit2399@users.noreply.github.com>
2021-10-16 12:44:12 +0530

    Update product-details.php
    Hi @divine-coder !!
    Ma'am, possibly your code had few errors related to PHP content.
    I have rectified some of them. 
    Kindly merge, if found useful.

--- product-details.php
+++ product-details.php
@@ -6,7 +6,33 @@ if(isset($_GET['action']) && $_GET['action']=="add"){
 	$id=intval($_GET['id']);
 	if(isset($_SESSION['cart'][$id])){
 		$_SESSION['cart'][$id]['quantity']++;
+	}else{
+		$sql_p="SELECT * FROM products WHERE id={$id}";
+		$query_p=mysqli_query($con,$sql_p);
+		if(mysqli_num_rows($query_p)!=0){
+			$row_p=mysqli_fetch_array($query_p);
+			$_SESSION['cart'][$row_p['id']]=array("quantity" => 1, "price" => $row_p['productPrice']);
+					echo "<script>alert('Product has been added to the cart')</script>";
+		echo "<script type='text/javascript'> document.location ='my-cart.php'; </script>";
+		}else{
+			$message="Product ID is invalid";
+		}
 	}
+}
+$pid=intval($_GET['pid']);
+if(isset($_GET['pid']) && $_GET['action']=="wishlist" ){
+	if(strlen($_SESSION['login'])==0)
+    {   
+header('location:login.php');
+}
+else
+{
+mysqli_query($con,"insert into wishlist(userId,productId) values('".$_SESSION['id']."','$pid')");
+echo "<script>alert('Product aaded in wishlist');</script>";
+header('location:my-wishlist.php');
+
+}
+}
 if(isset($_POST['submit']))
 {
 	$qty=$_POST['quality'];
@@ -284,13 +310,80 @@ $num=mysqli_num_rows($rt);
 								</div><!-- /.row -->		
 							</div><!-- /.rating-reviews -->
 <?php } ?>
-							
+							<div class="stock-container info-container m-t-10">
+								<div class="row">
+									<div class="col-sm-3">
+										<div class="stock-box">
+											<span class="label">Availability :</span>
+										</div>	
+									</div>
+									<div class="col-sm-9">
+										<div class="stock-box">
+											<span class="value"><?php echo htmlentities($row['productAvailability']);?></span>
+										</div>	
+									</div>
+								</div><!-- /.row -->	
+							</div>
+
+
+
+<div class="stock-container info-container m-t-10">
+								<div class="row">
+									<div class="col-sm-3">
+										<div class="stock-box">
+											<span class="label">Product Brand :</span>
+										</div>	
+									</div>
+									<div class="col-sm-9">
+										<div class="stock-box">
+											<span class="value"><?php echo htmlentities($row['productCompany']);?></span>
+										</div>	
+									</div>
+								</div><!-- /.row -->	
+							</div>
+
+
+<div class="stock-container info-container m-t-10">
+								<div class="row">
+									<div class="col-sm-3">
+										<div class="stock-box">
+											<span class="label">Shipping Charge :</span>
+										</div>	
+									</div>
+									<div class="col-sm-9">
+										<div class="stock-box">
+											<span class="value"><?php if($row['shippingCharge']==0)
+											{
+												echo "Free";
+											}
+											else
+											{
+												echo htmlentities($row['shippingCharge']);
+											}
+
+											?></span>
+										</div>	
+									</div>
+								</div><!-- /.row -->	
+							</div>
+
+							<div class="price-container info-container m-t-20">
+								<div class="row">
+									
+
+									<div class="col-sm-6">
+										<div class="price-box">
+											<span class="price">Rs. <?php echo htmlentities($row['productPrice']);?></span>
+											<span class="price-strike">Rs.<?php echo htmlentities($row['productPriceBeforeDiscount']);?></span>
+										</div>
+									</div>
+
 
 
 
 									<div class="col-sm-6">
-										<div class="favorite-button-23 m-t-10">
-											<a class="btn btn-pmary" data-toggle="tooltip" data-placement="right" title="Wishlist" href="product-details.php?pid=<?php echo htmlentities($row['id'])?>&&action=wishlist">
+										<div class="favorite-button m-t-10">
+											<a class="btn btn-primary" data-toggle="tooltip" data-placement="right" title="Wishlist" href="product-details.php?pid=<?php echo htmlentities($row['id'])?>&&action=wishlist">
 											    <i class="fa fa-heart"></i>
 											</a>
 											
@@ -306,7 +399,7 @@ $num=mysqli_num_rows($rt);
 
 
 
-							<div class="quantity-container-0 info-container">
+							<div class="quantity-container info-container">
 								<div class="row">
 									
 									<div class="col-sm-2">
@@ -402,7 +495,7 @@ while($rvw=mysqli_fetch_array($qry))
 										<form role="form" class="cnt-form" name="review" method="post">
 
 										
-										<div class="product-add-reviewers">
+										<div class="product-add-review">
 											<h4 class="title">Write your own review</h4>
 											<div class="review-table">
 												<div class="table-responsive">
@@ -463,11 +556,11 @@ while($rvw=mysqli_fetch_array($qry))
 																</div><!-- /.form-group -->
 															</div>
 
-															<div class="col-md-61">
+															<div class="col-md-6">
 																<div class="form-group">
 																	<label for="exampleInputReview">Review <span class="astk">*</span></label>
 
-<textarea class="form-control txt txt-review1" id="exampleInputReview" rows="4" placeholder="" name="review" required="required"></textarea>
+<textarea class="form-control txt txt-review" id="exampleInputReview" rows="4" placeholder="" name="review" required="required"></textarea>
 																</div><!-- /.form-group -->
 															</div>
 														</div><!-- /.row -->
@@ -520,14 +613,14 @@ while($rw=mysqli_fetch_array($qry))
 			
 		
 		<div class="product-info text-left">
-			<h3 class="name"><a href="product-details.php1?pid=<?php echo htmlentities($rw['id']);?>"><?php echo htmlentities($rw['productName']);?></a></h3>
+			<h3 class="name"><a href="product-details.php?pid=<?php echo htmlentities($rw['id']);?>"><?php echo htmlentities($rw['productName']);?></a></h3>
 			<div class="rating rateit-small"></div>
 			<div class="description"></div>
 
 			<div class="product-price">	
 				<span class="price">
 					Rs.<?php echo htmlentities($rw['productPrice']);?>			</span>
-										     <span class="price-before-discountss">Rs.
+										     <span class="price-before-discount">Rs.
 										     <?php echo htmlentities($rw['productPriceBeforeDiscount']);?></span>
 									
 			</div><!-- /.product-price -->
@@ -581,7 +674,7 @@ while($rw=mysqli_fetch_array($qry))
 	<script src="assets/js/bootstrap-slider.min.js"></script>
     <script src="assets/js/jquery.rateit.min.js"></script>
     <script type="text/javascript" src="assets/js/lightbox.min.js"></script>
-    <script src="assets/js/bootstrap-select.min.js1"></script>
+    <script src="assets/js/bootstrap-select.min.js"></script>
     <script src="assets/js/wow.min.js"></script>
 	<script src="assets/js/scripts.js"></script>
 
@@ -598,7 +691,7 @@ while($rw=mysqli_fetch_array($qry))
 			});
 		});
 
-		$(window).bind("load", function1() {
+		$(window).bind("load", function() {
 		   $('.show-theme-options').delay(2000).trigger('click');
 		});
 	</script>
